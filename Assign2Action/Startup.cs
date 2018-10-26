3624560376703d4df1bf0616fc98520d45bbd6f6
Satveer Dhillon <Satveerdhillon01@gmail.com>
2018-10-26 09:36:17 -0500

    Initial commit

---
+++ Assign2Action/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assign2Action.Startup))]
+namespace Assign2Action
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
